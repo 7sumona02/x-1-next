65ca2c222ed285a1b6bdd89ba4823191820598a1
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-03-09 05:12:36 +0530

    Initial commit from Create Next App

---
+++ app/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='bg-red-200 h-screen w-screen flex justify-center items-center'>
+        <h1>sumona</h1>
+    </div>
+  )
+}
+
+export default Section1
---
+++ app/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className='bg-violet-200 h-screen w-screen flex justify-center items-center'>
+        <h1>sumona</h1>
+    </div>
+  )
+}
+
+export default Section2
