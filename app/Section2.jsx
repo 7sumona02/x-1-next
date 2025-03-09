e0fab9aa72b2160ffb1d46db2f7120defd99ca3d
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-03-09 05:52:11 +0530

    added

--- app/Section2.jsx
+++ app/Section2.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Section2 = () => {
-  return (
-    <div className='bg-violet-200 h-screen w-screen flex justify-center items-center'>
-        <h1>sumona</h1>
-    </div>
-  )
-}
+  const { scrollYProgress: scrollYProgress2 } = useScroll();
+  const y2 = useTransform(scrollYProgress2, [0.4, 0.8], ['0%', '100%']);
 
-export default Section2
+  return (
+    <div className='bg-red-200 h-screen w-screen flex text-black'>
+      <div className='w-1/2 relative'>
+        <motion.div
+          className='h-1/2 bg-violet-400 absolute top-0 left-0 right-0'
+          style={{ y: y2 }}
+        >
+          hello
+        </motion.div>
+        <div className='h-1/4 bg-yellow-400'>hello</div>
+      </div>
+      <div className='h-screen bg-black w-1/2'>hello</div>
+    </div>
+  );
+};
+
+export default Section2;
--- app/Section3.jsx
+++ app/Section3.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
-const Section1 = () => {
-  const { scrollYProgress: scrollYProgress1 } = useScroll();
-  const y1 = useTransform(scrollYProgress1, [0, 0.4], ['0%', '100%']);
+const Section3 = () => {
+  const { scrollYProgress: scrollYProgress3 } = useScroll();
+  const y3 = useTransform(scrollYProgress3, [0.8, 1], ['0%', '100%']);
 
   return (
     <div className='bg-red-200 h-screen w-screen flex text-black'>
@@ -12,14 +12,14 @@ const Section1 = () => {
       <div className='w-1/2 relative'>
         <motion.div
           className='h-1/2 bg-violet-400 absolute top-0 left-0 right-0'
-          style={{ y: y1 }}
+          style={{ y: y3 }}
         >
           hello
         </motion.div>
-        <div className='h-1/4 bg-yellow-400'>hello</div>
+        <div className='h-1/2 bg-yellow-400'>hello</div>
       </div>
     </div>
   );
 };
 
-export default Section1;
+export default Section3;
